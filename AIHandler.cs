8383b85242ac8e9403f81e2b186dd5481ee6530d
kuba4g8 <kuba.holdys@gmail.com>
2025-10-05 17:02:39 +0200

    First commit

---
+++ AIHandler.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace nwmCoTo
+{
+    public class AIHandler
+    {
+
+
+        private string teachPrompt;
+        private class OllamaRequest
+        {
+            public string model {  get; set; }
+            public string prompt { get; set; }
+            public int max_tokens { get; set; }
+        }
+
+        private readonly HttpClient httpClient;
+
+        public AIHandler()
+        {
+            try
+            {
+                teachPrompt = File.ReadAllText("teachingPrompt.txt");
+                httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri("http://127.0.0.1:11434/");
+                httpClient.Timeout = TimeSpan.FromMinutes(10);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task<string> responseToMsg(string message)
+        {
+            try
+            {
+                OllamaRequest requestData = new OllamaRequest
+                {
+                    model = "llama2-uncensored:7b",
+                    prompt = message,
+                    max_tokens = 50,
+                };
+
+                // zmiana klasy na obiekt JSON
+                StringContent messageToBot = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
+
+                // Wyślij POST do endpointa /v1/completions
+                var response = await httpClient.PostAsync("v1/completions", messageToBot);
+
+
+                var responseText = await response.Content.ReadAsStringAsync();
+
+                using JsonDocument doc = JsonDocument.Parse(responseText);
+                JsonElement root = doc.RootElement;
+
+                string chatAnswer = root.GetProperty("choices")[0].GetProperty("text").GetString().Trim('"');
+
+                Console.WriteLine("AI odpowiada: " + chatAnswer);
+
+                // Wyrzuci wyjątek jeśli status != 2xx
+                if (chatAnswer == null)
+                {
+                    throw new Exception();
+                }
+                return chatAnswer;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                return "A weź spierdalaj";
+            }
+        }
+
+        public async Task teachModel()
+        {
+            try
+            {
+                OllamaRequest requestData = new OllamaRequest
+                {
+                    model = "llama2-uncensored:7b",
+                    prompt = teachPrompt,
+                    max_tokens = 200,
+                };
+
+                StringContent messageToBot = new StringContent(JsonSerializer.Serialize(requestData), Encoding.UTF8, "application/json");
+
+                // Wyślij POST do endpointa /v1/completions
+                var response = await httpClient.PostAsync("v1/completions", messageToBot);
+
+                // Debug: wypisz status i treść nawet jeśli nie 2xx
+                Console.WriteLine("Status code: " + response.StatusCode);
+                string responseText = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Response: " + responseText);
+
+                // Wyrzuci wyjątek jeśli status != 2xx
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using DotNetEnv;
+using Newtonsoft.Json.Bson;
+using nwmCoTo;
+using System.Threading.Channels;
+
+class Program
+{
+    private static DiscordSocketClient _client;
+    private static string token;
+    private static AIHandler ollama;
+    private static GatewayIntents intents =  GatewayIntents.GuildMembers | GatewayIntents.GuildMessages | GatewayIntents.DirectMessages;
+
+    private static bool isGenerating = false;
+
+    private static DiscordSocketConfig config = new DiscordSocketConfig
+    {
+        GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers | GatewayIntents.MessageContent
+    };
+
+
+    static string loadToken()
+    {
+        var token = File.ReadAllText("token.txt");
+
+        if (token==null)
+        {
+            Environment.Exit(0);
+        }
+        return token;
+    }
+
+
+    public static async Task Main()
+    {
+        ollama = new AIHandler();
+        //await ollama.teachModel();
+
+
+        _client = new DiscordSocketClient(config: config);
+
+        _client.Log += Log;
+
+        token = loadToken();
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        Console.WriteLine("GOTOWE!");
+        _client.MessageReceived += HandleRecivedMessage;
+        while (true)
+        {
+            
+        }
+    }
+
+
+    public static async Task HandleRecivedMessage(SocketMessage message)
+    {
+        if (message.Author.IsBot || isGenerating)
+            return;
+
+        ISocketMessageChannel Channel = message.Channel;
+
+        //DEBUG
+        Console.WriteLine($"{Channel.CachedMessages.ToString()}");
+        Console.WriteLine($"Kanał: {Channel.Name}");
+        //DEBUG
+
+        isGenerating = true;
+        Task<string> response = ollama.responseToMsg(message.Content);
+
+        response.Wait();
+        isGenerating = false;
+
+        await Channel.SendMessageAsync(text: response.Result);
+        Console.WriteLine(response.Result);
+    }
+
+    private static Task Log(LogMessage log)
+    {
+        Console.WriteLine(log.ToString());
+
+        return Task.CompletedTask;
+    }
+}
+
+
